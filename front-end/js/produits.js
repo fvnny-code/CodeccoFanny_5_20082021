796588e9ae2b33609190bdeeab116c29b65dab14
fvnny-code <f.codecco.grando@gmail.com>
2021-10-01 16:53:42 +0200

    style.css : modification des cartes produit + formulaire d'options produits.js : ajout des options de couleurs

--- front-end/js/produits.js
+++ front-end/js/produits.js
@@ -1,43 +1,50 @@
-
-(async function(){
-    
+(async function () {
     const teddyId = getTeddyId();
-    
-    const teddyData = await  getTeddyData(teddyId);
-    
-  
+    const teddyData = await getTeddyData(teddyId);
     displayProduct(teddyData);
-}) ()
-
-function getTeddyId(){
-    // return new URL (window.location.href).searchParams.get("id")
-    let params = (new URL(document.location)).searchParams;
-    let id = params.get('id');
+  })();
+  
+  function getTeddyId() {
+    let params = new URL(document.location).searchParams;
+    let id = params.get("id");
     return id;
-}
-
-function getTeddyData(teddyId){
+  }
+  
+  function getTeddyData(teddyId) {
     return fetch(`http://localhost:3000/api/teddies/${teddyId}`)
-    .then ((response) => {
+      .then((response) => {
         return response.json();
-    
-    })
-    .then ((teddyData)=>{
+      })
+      .then((teddyData) => {
         return teddyData;
-    })
-    .catch((error) =>{
-        alert("La connexion au serveur n'a pu être effectuée. veuillez réessayer un peu plus tard...")
-    })
-}
-
-function displayProduct(teddy) {
- 
-document.querySelector(".card__img").src = teddy.imageUrl;
-document.querySelector(".card__title").textContent = teddy.name;
-document.querySelector(".card__description").textContent = teddy.description;
-document.querySelector(".card__price").textContent = `${teddy.price / 100}.00 €`;
-
-}
-
-
-
+      //   données API
+      })
+      .catch((error) => {
+        alert(
+          "La connexion au serveur n'a pu être effectuée. veuillez réessayer un peu plus tard..."
+        );
+      });
+  }
+  
+  function displayProduct(teddy) {
+    document.querySelector(".card__img").src = teddy.imageUrl;
+    document.querySelector(".card__title").textContent = teddy.name;
+    document.querySelector(".card__description").textContent = teddy.description;
+    document.querySelector(".card__price").textContent = `${teddy.price / 100}.00 €`;
+  
+  
+    const selectColors = teddy.colors;
+    const colorForm = document.querySelector("#color--select");
+  
+    for (let i = 0; i < selectColors.length; i++) {
+        const option = document.createElement("option");
+        option.value = `${selectColors[i]}`;
+        option.innerHTML = `${selectColors[i]}`;
+        document.querySelector("#color--select").appendChild(option);
+        
+    }
+  
+    
+    
+  }
+  
